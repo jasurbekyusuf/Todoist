b4241c47543974506ba78b81b5ad99d102f9d3ec
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-06 02:36:39 -0800

    DATA: Model And Migrate Users

---
+++ Todoist.Api/Models/Users/User.cs
@@ -0,0 +1,22 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//==================================================
+
+using System;
+
+namespace Todoist.Api.Models.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+        public Guid? ManagerId { get; set; }
+    }
+}
