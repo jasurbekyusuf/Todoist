7be920c31234bfeea78a178abb81ae5ff98dd8e1
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-06 05:30:49 -0800

    CODE RUB: Add Header File

--- Todoist.Api.Tests.Unit/DeleteMe.cs
+++ Todoist.Api.Tests.Unit/DeleteMe.cs
@@ -1,3 +1,7 @@
+//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//==================================================
 
 using Xunit;
 
