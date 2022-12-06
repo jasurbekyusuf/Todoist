9e448e6c1ec534b2e2a6763c934eaef4bdb3bb6a
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-06 03:12:31 -0800

    DATA: Model & Migrate Team

---
+++ Todoist.Api/Brokers/Storages/IStorageBroker.Teams.cs
@@ -0,0 +1,16 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//==================================================
+
+using System.Threading.Tasks;
+using Todoist.Api.Models.Teams;
+using Todoist.Api.Models.Tickets;
+
+namespace Todoist.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Team> InsertTicketAsync(Team team);
+    }
+}
