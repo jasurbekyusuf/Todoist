ffa6e71dcca8db526970ab9c9280533447c2c8f5
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-07 04:41:20 -0800

    BROKERS: Select User By Id

--- Todoist.Api/Brokers/Storages/IStorageBroker.Users.cs
+++ Todoist.Api/Brokers/Storages/IStorageBroker.Users.cs
@@ -3,6 +3,7 @@
 // Free to use to bring order in your workplace
 //==================================================
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Todoist.Api.Models.Users;
@@ -13,6 +14,7 @@ namespace Todoist.Api.Brokers.Storages
     {
         ValueTask<User> InsertUserAsync(User user);
         IQueryable<User> SelectAllUsers();
+        ValueTask<User> SelectUserByIdAsync(Guid id);
         ValueTask<User> UpdateUserAsync(User user);
     }
 }
