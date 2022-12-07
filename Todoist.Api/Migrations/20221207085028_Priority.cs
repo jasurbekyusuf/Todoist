0fc6ac57f9a40f818c0e87cbc8d104aeb9d7fea2
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-07 00:51:59 -0800

    DATA: Priority Migration Add

---
+++ Todoist.Api/Migrations/20221207085028_Priority.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todoist.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Priority : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
