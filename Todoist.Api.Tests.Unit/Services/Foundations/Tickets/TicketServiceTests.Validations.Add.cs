097a62dfb4694d203d839e8f96da5c9c28fd92f8
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-08 01:02:13 -0800

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> WIP

---
+++ Todoist.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Validations.Add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todoist.Api.Tests.Unit.Services.Foundations.Tickets
+{
+    public class TicketServiceTests
+    {
+    }
+}
