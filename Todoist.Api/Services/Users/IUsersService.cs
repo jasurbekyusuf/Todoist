b31760006214f2fe45de76ebd8b717687606cfb0
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-09 12:20:47 -0800

    ShouldAddUserAsync -> FAIL

--- Todoist.Api/Services/Users/IUsersService.cs
+++ Todoist.Api/Services/Users/IUsersService.cs
@@ -1,6 +1,15 @@
-﻿namespace Todoist.Api.Services.Users
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//==================================================
+
+using System.Threading.Tasks;
+using Todoist.Api.Models.Users;
+
+namespace Todoist.Api.Services.Users
 {
     public interface IUsersService
     {
+        ValueTask<User> AddUserAsync(User user);
     }
 }
--- Todoist.Api/Services/Users/UsersService.cs
+++ Todoist.Api/Services/Users/UsersService.cs
@@ -1,6 +1,28 @@
-﻿namespace Todoist.Api.Services.Users
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//==================================================
+
+using System;
+using System.Threading.Tasks;
+using Todoist.Api.Brokers.Loggings;
+using Todoist.Api.Brokers.Storages;
+using Todoist.Api.Models.Users;
+
+namespace Todoist.Api.Services.Users
 {
-    public class UsersService
+    public class UsersService : IUsersService
     {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public UsersService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<User> AddUserAsync(User user) =>
+            throw new NotImplementedException();
     }
 }
