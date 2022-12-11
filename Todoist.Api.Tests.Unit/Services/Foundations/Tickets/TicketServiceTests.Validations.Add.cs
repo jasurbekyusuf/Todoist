14c3c47a340870704b518348256a86dcdcc98af3
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-11 00:44:16 -0800

    ShouldThrowValidationExceptionOnAddIfTicketIsInvalidAndLogItAysnc -> FAIL

--- Todoist.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Validations.Add.cs
+++ Todoist.Api.Tests.Unit/Services/Foundations/Tickets/TicketServiceTests.Validations.Add.cs
@@ -48,5 +48,71 @@ namespace Todoist.Api.Tests.Unit.Services.Foundations.Tickets
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfTicketIsInvalidAndLogItAysnc(
+            string invalidString)
+        {
+            // given
+            var invalidTicket = new Ticket
+            {
+                Title = invalidString,
+            };
+
+            var invalidTicketException = new InvalidTicketException();
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.Id),
+                values: "Id is required");
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.Title),
+                values: "Text is required");
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.Deadline),
+                values: "Value is required");
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.CreatedDate),
+                values: "Text is required");
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.UpdatedDate),
+                values: "Text is required");
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.CreatedUserId),
+                values: "Text is required");
+
+            invalidTicketException.AddData(
+                key: nameof(Ticket.UpdatedUserId),
+                values: "Text is required");
+
+            var expectedTicketValidationException =
+                new TicketValidationException(invalidTicketException);
+
+            // when
+            ValueTask<Ticket> addTicketTask = this.ticketService.AddTicketAsync(invalidTicket);
+
+            TicketValidationException actualValidationException =
+                await Assert.ThrowsAsync<TicketValidationException>(addTicketTask.AsTask);
+
+            //then
+            actualValidationException.Should().BeEquivalentTo(expectedTicketValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTicketValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertTicketAsync(It.IsAny<Ticket>()), Times.Once());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ Todoist.Api/Models/Tickets/Exceptions/InvalidTicketException.cs
@@ -0,0 +1,17 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//==================================================
+
+using Xeptions;
+
+namespace Todoist.Api.Models.Tickets.Exceptions
+{
+    public class InvalidTicketException : Xeption
+    {
+        public InvalidTicketException() 
+            : base(message: "Ticket is invalid.")
+        {
+        }
+    }
+}
