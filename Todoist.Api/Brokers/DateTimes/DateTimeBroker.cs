beaf20b9339966a0e9f84ab965630439f63f8b07
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-16 09:51:36 -0800

    BROKERS: Implement DateTime

---
+++ Todoist.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//==================================================
+
+using System;
+
+namespace Todoist.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTime() =>
+            DateTimeOffset.UtcNow;
+    }
+}
